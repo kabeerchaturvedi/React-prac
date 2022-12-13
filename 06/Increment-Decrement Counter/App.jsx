81b889e4a5ca552a7ea29b3afa4ae8be27939b13
kabeerchaturvedi <kbrchaturvedi@gmail.com>
2022-12-13 22:07:56 +0530

    Create App.jsx

---
+++ 06/Increment-Decrement Counter/App.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+
+function App() {
+    const [count, setCount] = useState(0);
+
+
+    return (
+        <div className="App">
+            <h1>Counter: <span>{count}</span></h1>
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <button onClick={() => setCount(count - 1)}>Decrement</button>
+        </div>
+    )
+}
+
+export default App
